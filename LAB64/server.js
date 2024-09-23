0eafbd13c21a1378997679f6c7af6b32ffb295fa
rsushizawa <rodrigo.syu@puccampinas.edu.br>
2024-09-23 13:46:49 -0300

    LAB64 - básico

---
+++ LAB64/server.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.end('<h1>Bem-vindo ao Node.js!</h1><p>Esta é a página inicial.</p>');
+    } else if (req.url === '/about') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.end('<h1>Sobre Nós</h1><p>Esta é a página sobre nós.</p>');
+    }else if (req.method === 'POST' && req.url === '/upload') {
+        let fileData = '';
+        req.on('data', chunk => {
+            fileData += chunk.toString();
+        });
+        req.on('end', () => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+            res.end('Upload simulado com sucesso!');
+        });
+    } else {
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        res.end('<h1>404 Não Encontrado</h1><p>A página que você está procurando não existe.</p>');
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Servidor rodando em http://${hostname}:${port}/`);
+});
