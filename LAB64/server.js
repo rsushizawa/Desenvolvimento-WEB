70fdcbc9159cd82a468d057aa0c2e8dd5a7f0541
rsushizawa <rodrigo.syu@puccampinas.edu.br>
2024-09-23 14:09:24 -0300

    Home é o LAB32

--- LAB64/server.js
+++ LAB64/server.js
@@ -1,12 +1,16 @@
 const http = require('http');
+var fs = require('fs');
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html; charset=utf-8');
-        res.end('<h1>Bem-vindo ao Node.js!</h1><p>Esta é a página inicial.</p>');
+        res.writeHead(200, { 'Content-Type':'text/html'});
+        var html = fs.readFileSync('./public/index.html');
+        res.end(html);
+        // res.setHeader('Content-Type', 'text/html; charset=utf-8');
+        // res.end('<h1>Bem-vindo ao Node.js!</h1><p>Esta é a página inicial.</p>');
     } else if (req.url === '/about') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html; charset=utf-8');
